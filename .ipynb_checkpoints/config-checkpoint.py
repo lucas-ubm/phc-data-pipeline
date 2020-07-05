81550d7ec4b37cfc8c725bb6af2c310665f07b20
Mavrokefalidis <lgubm23@gmail.com>
2020-07-05 10:54:11 +0200

    documents ordered

--- .ipynb_checkpoints/config-checkpoint.py
+++ .ipynb_checkpoints/config-checkpoint.py
@@ -1,1 +1,7 @@
-dir = "/Users/admin/Desktop/Thesis/Code/phc-data-pipeline/data/Processed/"
+
+
+#dir = '/home/kz035322/Thesis/phc-data-pipeline/data/Processed/'
+#guild = '/home/kz035322/Thesis/venv/.guild/'
+
+#dir = '/Users/admin/Desktop/Thesis/Code/phc-data-pipeline/data/Processed/'
+#guild = '/Users/admin/Desktop/Thesis/Code/phc-data-pipeline/venv/.guild/'
