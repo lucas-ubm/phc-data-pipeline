fb198265b598ac7dcbb1aad730cfb8075f2e8b2c
Mavrokefalidis <lgubm23@gmail.com>
2020-07-08 11:27:27 +0200

    erased ipynb checkpoints

--- .ipynb_checkpoints/README-checkpoint.md
+++
@@ -1,3 +0,0 @@
-# PHC Data pipeline
-
-In this repo I will work on integrating and combining three large pharmacogenomic studies (CTRP, CCLE and GDSC) to predict Drug Response based on gene expression. 
--- .ipynb_checkpoints/config-checkpoint.py
+++
@@ -1,7 +0,0 @@
-
-
-#dir = '/home/kz035322/Thesis/phc-data-pipeline/data/Processed/'
-#guild = '/home/kz035322/Thesis/venv/.guild/'
-
-#dir = '/Users/admin/Desktop/Thesis/Code/phc-data-pipeline/data/Processed/'
-#guild = '/Users/admin/Desktop/Thesis/Code/phc-data-pipeline/venv/.guild/'
